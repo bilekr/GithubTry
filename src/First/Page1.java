f8231027aa93c617278f01ee7c5a91363b24a634
bilekr <rbilek@hotmail.com>
2023-01-15 12:25:41 +0300

    first

---
+++ src/First/Page1.java
@@ -0,0 +1,7 @@
+package First;
+
+public class Page1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/First/Page3.java
@@ -0,0 +1,10 @@
+package First;
+
+public class Page3 {
+    public static void main(String[] args) {
+
+        System.out.println("deneme");
+
+    }
+
+}
